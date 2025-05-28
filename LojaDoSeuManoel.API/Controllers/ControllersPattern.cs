39d14f05968d03380855b2d051736da2227ca108
Paulo Andrade <parthur207@gmail.com>
2025-05-28 19:49:42 -0300

    Pre-Configuração do relacionamento de entidades.
    Criação do controller padrão.

---
+++ LojaDoSeuManoel.API/Controllers/ControllersPattern.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaDoSeuManoel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllersPattern : ControllerBase
+    {
+    }
+}
--- LojaDoSeuManoel.Infrastruture/Persistense/LojaDoSeuManoelDbContext.cs
+++ LojaDoSeuManoel.Infrastruture/Persistense/LojaDoSeuManoelDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LojaDoSeuManoel.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LojaDoSeuManoel.Infrastruture.Persistense
 {
@@ -8,9 +9,34 @@ namespace LojaDoSeuManoel.Infrastruture.Persistense
             : base(options)
         {
         }
+
+        public DbSet<BoxEntity> Box {get;set;}
+        public DbSet<CustomerEntity> Customer { get; set; }
+        public DbSet<ProductGameEntity> ProductGame { get; set; }
+        public DbSet<OrderEntity> Order { get; set; }
+        public DbSet<OrderProductGameEntity> OrderProductGame { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BoxEntity>
+                (
+                x => x.HasKey(x => x.Id));
+
+            modelBuilder.Entity<CustomerEntity>(x =>
+            {
+                x.HasKey(x => x.Id);
+
+                x.HasMany(x => x.OrderList)
+                    .WithOne(x => x.Costumer)
+                    .HasForeignKey(x => x.CustomerId)//Chave estrangeira
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<ProductGameEntity>(x =>
+            {
+                x.HasKey(x => x.Id);//Chave primária
+            });
+
+
         }
 
     }
