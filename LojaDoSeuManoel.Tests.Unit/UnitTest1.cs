1c57ae4fbede00bf61216904f4170c0866ee0a43
Paulo Andrade <parthur207@gmail.com>
2025-05-30 19:47:07 -0300

    Implementação do projeto de teste unitário para validação das funcionalidades.

---
+++ LojaDoSeuManoel.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LojaDoSeuManoel.Tests.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
